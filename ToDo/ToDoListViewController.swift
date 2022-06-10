13aac9371453c1b2b6fdd541a251f2f6aa1899b5
tanaka <tanaka.mawoyo@yahoo.com>
2022-06-10 17:48:17 +0200

    Use local data for Persistence

--- ToDo/ToDoListViewController.swift
+++ ToDo/ToDoListViewController.swift
@@ -9,13 +9,13 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
     
-    var itemArray = ["buy milk", "study", "hello"]
-    
+    let itemArray = ["buy milk", "study", "hello"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
+
     
     //MARK: - TableView Datasource Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -35,34 +35,13 @@ class ToDoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        tableView.cellForRow(at: indexPath)?.accessoryType = .none
         }else{
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
-    
-    //MARK: - Add new Items
-    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-        var textField = UITextField()
-        
-        let alert = UIAlertController(title: "Add new task", message: "", preferredStyle: .alert)
-        
-        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            //what happens when user clicks add item button on the UIAlert
-            self.itemArray.append(textField.text!)
-            
-            self.tableView.reloadData()
-        }
-        //add textfield in the alert
-        alert.addTextField { (alertTextField) in
-            alertTextField.placeholder = "create new task"
-            textField = alertTextField
-        }
-        
-        alert.addAction(action)
-        present(alert, animated: true, completion: nil)
-    }
+
 }
 
